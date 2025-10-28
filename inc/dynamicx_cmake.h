76970fac0a8bbf91d8b0233c7d707a6d75b9d08b
wiselook <1656438881@qq.com>
2025-10-28 23:20:15 +0800

    Feat: add dynamicx_cmake.h.

---
+++ inc/dynamicx_cmake.h
@@ -0,0 +1,10 @@
+//
+// Created by wk on 2025/10/28.
+//
+
+#ifndef CMAKE_HOMEWORK_DYNAMICX_CMAKE_H
+#define CMAKE_HOMEWORK_DYNAMICX_CMAKE_H
+
+void print_dx(std::string number);
+
+#endif // CMAKE_HOMEWORK_DYNAMICX_CMAKE_H
